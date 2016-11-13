a27f02088aa4222c7de415e045fa0a06f4557eb3
Toby Salway <tobymsalway@gmail.com>
2016-11-13 11:53:34 +0000

    General tidy up.  Changed to remove redundant variables and using the pipe (%>%) operator.

--- run_analysis.R
+++ run_analysis.R
@@ -1,88 +1,73 @@
+## Load the required packages.
 library(dplyr)
 library(tidyr)
 
-## Read in the features dataset
-features <- read.delim("/Users/tobysalway/data/Getting and Cleaning Data/UCI HAR Dataset/features.txt", sep = "", header = FALSE, colClasses = "character")
+## Set the working directory
+setwd("~/data/Getting and Cleaning Data/UCI HAR Dataset")
 
-## Create an index of measures relating to mean or standard deviation.
-featuresind <- grep("mean|std",features$V2)
+## Read in the y test and train datasets
+ytest <- read.delim("./test/Y_test.txt", sep = "", header = FALSE, colClasses = "character")
+ytrain <- read.delim("./train/Y_train.txt", sep = "", header = FALSE, colClasses = "character")
 
-## Use the index to select thoses rows with features relating to the mean and standard deviatino.
+## Read in the activity dataset and rename columns
+activity_labels <- read.delim("./activity_labels.txt", sep = "", header = FALSE, colClasses = "character")
+activity_labels <- rename(activity_labels, activityid = V1, activityname = V2)
+
+## Read in the features dataset and rename columns
+features <- read.delim("./features.txt", sep = "", header = FALSE, colClasses = "character")
+features <- rename(features, featureid = V1, featurename = V2)
+
+## Create an index of feature measures relating to mean or standard deviation.
+featuresind <- grep("mean|std",features$featurename)
+
+## Use the index to select thoses rows with features relating to the mean and standard deviation.
 selectedfeatures <- features[featuresind,]
 
-## Rename the selected features to clarify their meaning
-selectedfeatures <- rename(selectedfeatures, featureid = V1, featurename = V2)
-
 ## Read in the test and training subject datasets
-subject_test <- read.delim("/Users/tobysalway/data/Getting and Cleaning Data/UCI HAR Dataset/test/subject_test.txt", sep = "", header = FALSE, colClasses = "character")
-subject_train <- read.delim("/Users/tobysalway/data/Getting and Cleaning Data/UCI HAR Dataset/train/subject_train.txt", sep = "", header = FALSE, colClasses = "character")
+subject_test <- read.delim("./test/subject_test.txt", sep = "", header = FALSE, colClasses = "character")
+subject_train <- read.delim("./train/subject_train.txt", sep = "", header = FALSE, colClasses = "character")
 
 ## Rename the attributes to be clearer
 subject_test <- rename(subject_test, subjectid = V1)
 subject_train <- rename(subject_train, subjectid = V1)
- 
-## Read in the x test data
-xtest <- read.delim("/Users/tobysalway/data/Getting and Cleaning Data/UCI HAR Dataset/test/X_test.txt", sep = "", header = FALSE, colClasses = "character")
-xtrain <- read.delim("/Users/tobysalway/data/Getting and Cleaning Data/UCI HAR Dataset/train/X_train.txt", sep = "", header = FALSE, colClasses = "character")
 
-## Turn the tables into tibbles
-testtable <- tbl_df(xtest)
-traintable <- tbl_df(xtrain)
- 
+## Read in the x test and train data and convert to tibbles
+testtable <- tbl_df(read.delim("./test/X_test.txt", sep = "", header = FALSE, colClasses = "character"))
+traintable <- tbl_df(read.delim("./train/X_train.txt", sep = "", header = FALSE, colClasses = "character"))
+
 ## Add the subject data to the tables
 testtable <- cbind(testtable, subject_test)
 traintable <- cbind(traintable, subject_train)
- 
-## Pivot the measurement columns to be rows using gather
-testtable2 <- testtable %>% gather(featureid, value, V1:V561)
-traintable2 <- traintable %>% gather(featureid, value, V1:V561)
- 
-## Extract the activityids from both sets of data
-testtable3 <- mutate(testtable2, activityid = substr(value,nchar(value)-2,nchar(value)), value = substr(value,1,nchar(value)-4))
-traintable3 <- mutate(traintable2, activityid = substr(value,nchar(value)-2,nchar(value)), value = substr(value,1,nchar(value)-4))
 
-## Read in the y test and train datasets
-ytest <- read.delim("/Users/tobysalway/data/Getting and Cleaning Data/UCI HAR Dataset/test/Y_test.txt", sep = "", header = FALSE, colClasses = "character")
-ytrain <- read.delim("/Users/tobysalway/data/Getting and Cleaning Data/UCI HAR Dataset/train/Y_train.txt", sep = "", header = FALSE, colClasses = "character")
- 
-## Read in the activity dataset and cleanse
-activity_labels <- read.delim("/Users/tobysalway/data/Getting and Cleaning Data/UCI HAR Dataset/activity_labels.txt", sep = "", header = FALSE, colClasses = "character")
-activity_labels <- rename(activity_labels, activityid = V1, activityname = V2)
- 
-testtable4 <- mutate(testtable3, featureid = sub("V","",testtable3$featureid))
-traintable4 <- mutate(traintable3, featureid = sub("V","",traintable3$featureid))
- 
-mergedtesttable <- merge(testtable4, selectedfeatures, by.x="featureid", by.y="featureid")
-mergedtraintable <- merge(traintable4, selectedfeatures, by.x="featureid", by.y="featureid")
- 
-## Make the activityid numeric
-mergedtesttable <- mutate(mergedtesttable, activityid = as.numeric(activityid))
-mergedtraintable <- mutate(mergedtraintable, activityid = as.numeric(activityid))
- 
-## Merge the activity information with the training table data
-mergedtesttable2 <- merge(mergedtesttable, activity_labels, by.x="activityid", by.y="activityid")
-mergedtraintable2 <- merge(mergedtraintable, activity_labels, by.x="activityid", by.y="activityid")
- 
-## Concatenate the data together.
-finaltable <- rbind(mergedtesttable2, mergedtraintable2)
+## For both the test and training data do the following:
+## 1. Pivot the measurement columns to be rows using gather.
+## 2. Cleanse and replace the activityid and featureid columns.
+## 3. Merge with the features and activity labels data.
+testtable2 <- testtable %>% 
+    gather(featureid, value, V1:V561) %>%
+    mutate(activityid = as.numeric(substr(value,nchar(value)-2,nchar(value))), value = substr(value,1,nchar(value)-4), featureid = sub("V","",featureid)) %>%
+    merge(selectedfeatures, by.x="featureid", by.y="featureid") %>%
+    merge(activity_labels, by.x="activityid", by.y="activityid")
 
-## Change the activity measurements to be numeric
-finaltable2 <- mutate(finaltable, value = as.numeric(value))
+traintable2 <- traintable %>% 
+    gather(featureid, value, V1:V561) %>%
+    mutate(activityid = as.numeric(substr(value,nchar(value)-2,nchar(value))), value = substr(value,1,nchar(value)-4), featureid = sub("V","",featureid)) %>%
+    merge(selectedfeatures, by.x="featureid", by.y="featureid") %>%
+    merge(activity_labels, by.x="activityid", by.y="activityid")
 
-## Group the data by activity, feature and subject.
-grouping <- group_by(finaltable2, activityname, featurename, subjectid)
+## Concatenate the test and training data together.
+finaltable <- rbind(testtable2, traintable2)
 
-## Create a final dataset
-summarizing <- summarise(grouping, meanvalue = mean(value))
+## Perform the following tidying on the concatanted data:
+## 1. Convert value to be numeric. 
+## 2. Group the data by activity, feature and subject.  
+## 3. Summarise the data to get the mean values.
+## 4. Arrange the data by subject, activity and features
+tidytable <- mutate(finaltable, value = as.numeric(value)) %>%
+    group_by(activityname, featurename, subjectid) %>%
+    summarise(meanvalue = mean(value)) %>%
+    arrange(subjectid, activityname, featurename) %>%
+    select(subjectid, featurename, activityname, meanvalue)
 
-## Arrange the data.
-summarizing <- arrange(summarizing, subjectid, activityname, featurename)
-
-tidytable <- select(summarizing, subjectid, featurename, activityname, meanvalue)
-
-## Lastly write out the summarised dataset in csv format.
-write.csv(tidytable,file="./tidydata.csv", row.names=FALSE)
-
-
-     
- 
+## Lastly write out the summarised tidy dataset in csv format.
+write.csv(tidytable,file="~/git/gettingandcleaningdata/tidydata2.csv", row.names=FALSE)
