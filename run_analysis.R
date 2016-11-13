d37da786d74cab6ee5da4d9d289a288ebf45269a
Toby Salway <tobymsalway@gmail.com>
2016-11-13 20:54:11 +0000

    Spread the data at the end to make the variables column names again.

--- run_analysis.R
+++ run_analysis.R
@@ -30,8 +30,8 @@ featuresind <- grep("mean|std",features$featurename)
 selectedfeatures <- features[featuresind,]
 
 ## Read in the test and training subject datasets
-subjecttest <- read.delim("./test/subject_test.txt", sep = "", header = FALSE, colClasses = "character")
-subjecttrain <- read.delim("./train/subject_train.txt", sep = "", header = FALSE, colClasses = "character")
+subjecttest <- read.delim("./test/subject_test.txt", sep = "", header = FALSE, colClasses = "numeric")
+subjecttrain <- read.delim("./train/subject_train.txt", sep = "", header = FALSE, colClasses = "numeric")
 
 ## Rename the attributes to be clearer
 subjecttest <- rename(subjecttest, subjectid = V1)
@@ -47,19 +47,24 @@ traintable <- cbind(traintable, subjecttrain, activitytrain)
 
 ## For both the test and training data do the following:
 ## 1. Pivot the measurement columns to be rows using gather.
-## 2. Cleanse and replace the activityid and featureid columns.
-## 3. Merge with the features and activity labels data.
+## 2. Cleanse and replace the featureid column.  Merge with the features and cleanse the feature names.
+## 3. Merge with the activity labels data.
+## 4. Select to exclude the feature and activity id columns
 testtable <- testtable %>% 
     gather(featureid, value, V1:V561) %>%
     mutate(featureid = sub("V","",featureid)) %>%
     merge(selectedfeatures, by.x="featureid", by.y="featureid") %>%
-    merge(activitylabels, by.x="activityid", by.y="activityid")
+    mutate(featurename = gsub("[-\\(\\)]","",featurename)) %>%
+    merge(activitylabels, by.x="activityid", by.y="activityid") %>%
+    select(subjectid, featurename, activityname, value)
 
 traintable <- traintable %>% 
     gather(featureid, value, V1:V561) %>%
     mutate(featureid = sub("V","",featureid)) %>%
     merge(selectedfeatures, by.x="featureid", by.y="featureid") %>%
-    merge(activitylabels, by.x="activityid", by.y="activityid")
+    mutate(featurename = gsub("[-\\(\\)]","",featurename)) %>%
+    merge(activitylabels, by.x="activityid", by.y="activityid") %>%
+    select(subjectid, featurename, activityname, value)
 
 ## Concatenate the test and training data together.
 finaltable <- rbind(testtable, traintable)
@@ -68,12 +73,13 @@ finaltable <- rbind(testtable, traintable)
 ## 1. Convert value to be numeric. 
 ## 2. Group the data by activity, feature and subject.  
 ## 3. Summarise the data to get the mean feature value for each distinct subject, activitity and feature.
-## 4. Arrange the data by subject, activity and features
+## 4. Spread to put the featurenames back to column names. 
+## 5. Arrange the data by subject and activity.
 tidytable <- mutate(finaltable, value = as.numeric(value)) %>%
     group_by(activityname, featurename, subjectid) %>%
     summarise(meanvalue = mean(value)) %>%
-    arrange(subjectid, activityname, featurename) %>%
-    select(subjectid, featurename, activityname, meanvalue)
+    spread(featurename, meanvalue) %>%
+    arrange(subjectid, activityname)
 
 ## Lastly write out the summarised tidy dataset in txt format.
 write.table(tidytable, file="~/tidyhardata.txt", row.names=FALSE)
